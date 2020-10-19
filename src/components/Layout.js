81077f31bb3e176ae1d43f431410d53b42b52548
whois-this <46181010+whois-this@users.noreply.github.com>
2020-10-19 21:38:28 +0530

    favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,6 +15,11 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+                    <link rel="manifest" href="/images/favicon/site.webmanifest"></link>
                 </Head>
                 <div id="page" className={'site palette-' + _.get(this.props, 'data.config.palette', null)}>
                   <Header {...this.props} />
