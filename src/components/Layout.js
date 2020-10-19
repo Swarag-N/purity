f3109bdd95c5f2d6e483b0ef4f7793db17aa6af7
whois-this <46181010+whois-this@users.noreply.github.com>
2020-10-19 21:41:08 +0530

    Bug Fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,6 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
